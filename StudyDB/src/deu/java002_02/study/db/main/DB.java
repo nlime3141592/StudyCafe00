9fb8ebb7ca2af5d8b8a7754ce1cca8da0eee528a
nlime3141592 <nlime3141592@naver.com>
2023-11-25 23:41:05 +0900

    서비스 큐 삭제

--- StudyDB/src/deu/java002_02/study/db/main/DB.java
+++ StudyDB/src/deu/java002_02/study/db/main/DB.java
@@ -32,6 +32,8 @@ public class DB extends StudyThread
 		{
 			while(m_conQueue.size() > 0)
 			{
+				// NOTE: DB 동시성 문제 해결을 위해 넣었던 Queue 자료구조이나, 오류 발생으로 인해 주석처리 하였습니다.
+				/*
 				// NOTE: 3초의 작업 시간을 줍니다. 만약 3초 안에 작업을 끝내지 못했다면, DB Connection이 강제로 종료됩니다.
 				long nowTime = System.nanoTime();
 				long endTime = nowTime + (long)(1e+9 * 3);
@@ -42,6 +44,8 @@ public class DB extends StudyThread
 					nowTime = System.nanoTime();
 
 				m_conQueue.poll().bindConnectionModule(null);
+				*/
+				m_conQueue.poll().bindConnectionModule(m_conModule);
 			}
 		}
 	}
